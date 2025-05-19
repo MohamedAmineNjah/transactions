7763ada64581497a8aebb3dba8708e29b440ff04
MohamedAmineNjah <mohamedamine.njah@esprit.tn>
2025-05-19 11:18:45 +0200

    unit test

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -14,5 +14,24 @@ import org.mockito.Mock;
 @SpringBootTest
 class DemoApplicationTests {
 
+	@Mock
+	private TransactionRepository transactionRepository;
+
+	@InjectMocks
+	private TransactionService transactionService;
+
+	@Test
+	void testFindTransactionsById() {
+		Transaction transaction1 = new Transaction("1", "2", "Reception", LocalDateTime.now());
+		Transaction transaction2 = new Transaction("2", "3", "Duplicate", LocalDateTime.now());
+
+		when(transactionRepository.findByPrimaryIdOrSecondaryId("8MHFNAuOGS", "8MHFNAuOGS"))
+				.thenReturn(Arrays.asList(transaction1, transaction2));
+
+		List<Transaction> transactions = transactionService.findTransactionsById("8MHFNAuOGS");
+
+		assertEquals(2, transactions.size());
+		assertEquals("Reception", transactions.get(0).getEventType());
+	}
 
 }
