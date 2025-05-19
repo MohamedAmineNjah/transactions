f28b0f48a708a1365c6adb6a168b271b2d18f78a
MohamedAmineNjah <mohamedamine.njah@esprit.tn>
2025-05-19 22:40:05 +0200

    crossOr

--- src/main/java/com/example/demo/TransactionController.java
+++ src/main/java/com/example/demo/TransactionController.java
@@ -1,10 +1,7 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,6 +12,7 @@ public class TransactionController {
     @Autowired
     private TransactionService transactionService;
 
+    @CrossOrigin(maxAge = 3600)
     @GetMapping("/{id}")
     public List<Transaction> getTransactions(@PathVariable String id) {
         return transactionService.findTransactionsById(id);
